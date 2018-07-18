4389defd8b74f36bc4575f9dbf652aefb6da04dc
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-18 22:18:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,6 +87,6 @@ function placeOrder(cardNumber) {
   {
     var totalCost = total()
     cart.length = 0
-    return `Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
