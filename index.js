e52f26550e9293ca1803249df962e1f673fd0f49
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-18 18:05:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,17 @@ function total() {
 }
 
 function removeFromCart(item) {
-  
+  for (let i = 0; i < cart.length; i++)
+  {
+    if (cart[i].itemName != item)
+    {
+      return "That item is not in your cart."
+    }
+    else
+    {
+      
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
