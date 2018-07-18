a2d1484aa2605095a3daa865ea51a0dcb49112c8
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-18 22:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 0)
+  if (cardNumber === null)
   {
     return "Sorry, we don't have a credit card on file for you."
   }
