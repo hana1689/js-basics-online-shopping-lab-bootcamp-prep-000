65838c4dd9ef6c81ee28e1718872d2d20e756930
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-18 17:09:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,16 +23,31 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  var string = ""
   if (cart.length === 0)
   {
     return "Your shopping cart is empty."
   }
   else
   {
-    for (let i = 0; i < cart.length; i++)
+    if (cart.length == 1)
+    {
+      string = `${cart[0].itemName} at ${cart[0].itemPrice}.`
+    }
+    else if (cart.length == 2)
+    {
+      for (let i = 0; i < cart.length; i++)
       {
-        return `In your cart, you have ${cart[i].itemName}, `
+        string += `${cart[i].itemName} at ${cart[i].itemPrice}, and`
       }
+    }
+    else
+    {
+      for (let i = 0; i < cart.length; i++)
+        {
+        return `In your cart, you have ${cart[i].itemName}, `
+        }
+    }
   }
 }
 
